ed487eca3a66b40ad4147cfaae474381c62d2cc2
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 17:15:29 +0300

    access token not needed for now

--- src/user/schemas/createUser.schema.ts
+++ src/user/schemas/createUser.schema.ts
@@ -4,12 +4,10 @@ export default Joi.object({
   username: Joi.string().alphanum().min(3).max(20).required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
   repeatPassword: Joi.ref('password'),
-  accessToken: [Joi.string(), Joi.number()],
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ['com', 'net'] },
   }),
 })
   .with('username', 'password')
-  .with('password', 'repeatPassword')
-  .xor('accessToken', 'password');
+  .with('password', 'repeatPassword');
