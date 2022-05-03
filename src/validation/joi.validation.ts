999e9313ad5fd1af4e3c251d3a567e0245ff1631
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 17:14:57 +0300

    sending a more detailed message

--- src/validation/joi.validation.ts
+++ src/validation/joi.validation.ts
@@ -8,7 +8,7 @@ export class JoiValidationPipe implements PipeTransform {
   transform(value: any) {
     const { error } = this.schema.validate(value);
     if (error) {
-      throw new BadRequestException('Validation Failed');
+      throw new BadRequestException(error.details[0].message);
     }
     return value;
   }
