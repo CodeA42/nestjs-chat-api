5de179399a034837e5b23f74c9f89b26e605c7a9
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 14:36:48 +0300

    using validation pipe now

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { JoiValidationPipe } from 'src/validation/joi.validation';
+import { CreateUserDto } from './dto/user.dto';
+import createUserSchema from './schemas/createUser.schema';
 import { UserService } from './user.service';
 
 @Controller('')
@@ -6,12 +9,9 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('/register')
-  register(
-    @Body('username') username: string,
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ): any {
-    const response = this.userService.register(username, email, password);
+  @UsePipes(new JoiValidationPipe(createUserSchema))
+  register(@Body() createUserDto: CreateUserDto): any {
+    const response = this.userService.register(createUserDto);
     return response;
   }
 }
