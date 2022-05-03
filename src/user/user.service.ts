eee8e9510b82128514bfc38a32e7c997f58a1af1
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 15:26:21 +0300

    added register functionality

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/user.dto';
 import User from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 Injectable();
 export class UserService {
@@ -12,8 +18,19 @@ export class UserService {
     private configService: ConfigService,
   ) {}
 
-  register(userDto: CreateUserDto) {
-    return 'here';
+  async register(userDto: CreateUserDto) {
+    try {
+      await this.selectByEmail(userDto.email);
+      throw new HttpException('Email Exists', HttpStatus.CONFLICT);
+    } catch (e) {}
+
+    try {
+      await this.selectByUsername(userDto.username);
+      throw new HttpException('Username exists', HttpStatus.CONFLICT);
+    } catch (e) {}
+
+    const user = await this.hashPaswordAndInsertUser(userDto);
+    return user.id;
   }
 
   // login() {}
@@ -21,4 +38,55 @@ export class UserService {
   // refresh() {}
 
   // logout() {}
+
+  private async hashPaswordAndInsertUser(userDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(
+      userDto.password,
+      Number(this.configService.get<number>('saltRounds')),
+    );
+    return await this.insertUser(
+      userDto.username,
+      hashedPassword,
+      userDto.email,
+    );
+  }
+
+  private async insertUser(username: string, password: string, email: string) {
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    user.email = email;
+
+    return await this.usersRepository.save(user);
+  }
+
+  private async selectByEmail(email: string): Promise<User | null> {
+    try {
+      const user: User = await this.usersRepository.findOne({
+        where: {
+          email,
+        },
+      });
+      if (user) return user;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+    throw new NotFoundException();
+  }
+
+  private async selectByUsername(username: string): Promise<User | null> {
+    try {
+      const user: User = await this.usersRepository.findOne({
+        where: {
+          username,
+        },
+      });
+      if (user) return user;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+    throw new NotFoundException();
+  }
 }
