f8b7794fc8f1c359aee7fadd98be03f58f930565
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 14:10:38 +0300

    user joi schema

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+export default Joi.object({
+  username: Joi.string().alphanum().min(3).max(20).required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  repeatPassword: Joi.ref('password'),
+  accessToken: [Joi.string(), Joi.number()],
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+})
+  .with('password', 'repeatPassword')
+  .xor('accessToken', 'password');
