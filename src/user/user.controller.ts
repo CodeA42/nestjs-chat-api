198a8d1fcef00b33b80025cc89ff77a5ad35c2c6
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 16:25:24 +0300

    added link to login renamed createUserDto to userAuthDto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { JoiValidationPipe } from 'src/validation/joi.validation';
-import { CreateUserDto } from './dto/user.dto';
+import { UserAuthDto } from './dto/userAuthDto';
 import createUserSchema from './schemas/createUser.schema';
 import { UserService } from './user.service';
 
@@ -10,8 +10,13 @@ export class UserController {
 
   @Post('/register')
   @UsePipes(new JoiValidationPipe(createUserSchema))
-  register(@Body() createUserDto: CreateUserDto): any {
-    const response = this.userService.register(createUserDto);
-    return response;
+  register(@Body() userAuthDto: UserAuthDto): any {
+    return this.userService.register(userAuthDto);
+  }
+
+  @Post('login')
+  @UsePipes(new JoiValidationPipe(createUserSchema))
+  login(@Body() userAuthDto: UserAuthDto): any {
+    return this.userService.login(userAuthDto);
   }
 }
