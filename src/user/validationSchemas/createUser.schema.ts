53b2e837fe48ce40a0a988a702baf1050ffd4763
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-03 17:46:54 +0300

    renamed the folder the schema will now be used only for register

---
+++ src/user/validationSchemas/createUser.schema.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  username: Joi.string().alphanum().min(3).max(20).required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+});
