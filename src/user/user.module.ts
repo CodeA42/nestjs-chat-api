fb4f65311497ec1554a6db4c4638529f049fdf07
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-04 14:18:31 +0300

    added the middleware to the refresh route

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Token from './entities/Token.entity';
 import User from './entities/User.entity';
+import { RefreshTokenValidator } from './middlewares/refreshTokenValidator.middleware';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
@@ -11,4 +17,10 @@ import { UserService } from './user.service';
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RefreshTokenValidator)
+      .forRoutes({ path: 'refresh', method: RequestMethod.GET });
+  }
+}
