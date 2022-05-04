50761432a7750b0066f58461bd9a867edf5f1817
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-04 11:16:21 +0300

    added db connection to the root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,25 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import Token from './user/entities/Token.entity';
+import User from './user/entities/User.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ConfigModule.forRoot(), UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nestapiuser',
+      password: 'megasecret',
+      database: 'nestapidb',
+      entities: [Token, User],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
