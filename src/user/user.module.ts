9d461f07a98ec540b532a654b7c941912f14da5b
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-04 13:06:37 +0300

    need the token

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import User from './entities/user.entity';
+import Token from './entities/Token.entity';
+import User from './entities/User.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([User, Token]), ConfigModule],
   controllers: [UserController],
   providers: [UserService],
 })
