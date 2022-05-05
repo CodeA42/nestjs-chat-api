6bd4f5d49427fda12963498842561d55287d7b07
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-05 10:43:36 +0300

    old types

--- src/types/AuthTypes.ts
+++
@@ -1,6 +0,0 @@
-export class AuthTypes {
-  static ACCESS = 'accessToken';
-  static REFRESH = 'refreshToken';
-  static ACCESS_SECRET = 'ACCESS_TOKEN_SECRET';
-  static REFRESH_SECRET = 'REFRESH_TOKEN_SECRET';
-}
