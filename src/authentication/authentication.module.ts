3a4d6fb19c01f97b39e5091fa7a532b576c08aa2
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-05 11:40:44 +0300

    authentication module is now global

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Token from '../entities/Token.entity';
@@ -6,9 +6,11 @@ import User from '../entities/User.entity';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([User, Token]), ConfigModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
+  exports: [AuthenticationService],
 })
 export class AuthenticationModule {}
