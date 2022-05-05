83ae38a2bbe63da26b6ca62dec196c382d887c0c
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-05 17:14:35 +0300

    config module was missing

--- src/chat/Chat.module.ts
+++ src/chat/Chat.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ChatController } from './Chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './Chat.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
