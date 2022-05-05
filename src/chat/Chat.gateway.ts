d05be1c9b12877ba13dbd9906ca0d6af02a75bce
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-05 17:14:58 +0300

    basic gateway

--- src/chat/Chat.gateway.ts
+++ src/chat/Chat.gateway.ts
@@ -1,13 +1,45 @@
+import { Logger, UseGuards } from '@nestjs/common';
 import {
+  ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { AuthTypes } from 'src/@types/AuthTypes';
+import { Authentication } from 'src/decorators/Authentication.decorator';
+import { AuthenticationGuard } from 'src/guards/Authentication.guard';
 
-@WebSocketGateway(80, { namespace: 'chat' })
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string): string {
-    return message;
+@WebSocketGateway(80, { cors: '*' })
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger: Logger = new Logger('ChatGateway');
+  afterInit(server: Server) {
+    this.logger.log('Initialized!');
+  }
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`);
+  }
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+  @WebSocketServer()
+  server: Server;
+
+  // @Authentication(AuthTypes.ACCESS)
+  // @UseGuards(AuthenticationGuard)
+  @SubscribeMessage('join')
+  joinRoom(client: Socket, message: string): WsResponse<string> {
+    // console.log(socket);
+
+    // console.log(message);
+
+    return { event: 'message', data: message };
   }
 }
