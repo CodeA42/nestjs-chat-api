05a61f8ab1f415d016aef130a8d49a39a5377358
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-05 11:38:49 +0300

    some additions

--- src/@types/AuthTypes.ts
+++ src/@types/AuthTypes.ts
@@ -3,4 +3,6 @@ export class AuthTypes {
   static REFRESH = 'refreshToken';
   static ACCESS_SECRET = 'ACCESS_TOKEN_SECRET';
   static REFRESH_SECRET = 'REFRESH_TOKEN_SECRET';
+  static ACCESS_DURATION = 'ACCESS_TOKEN_DURATION';
+  static REFRESH_DURATION = 'REFRESH_TOKEN_DURATION';
 }
