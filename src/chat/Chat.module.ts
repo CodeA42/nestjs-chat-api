53d9baf13743047ec4d24e9bb6b92b087e6d753f
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-09 21:54:40 +0300

    linked admin module

--- src/chat/Chat.module.ts
+++ src/chat/Chat.module.ts
@@ -3,12 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Chat from 'src/entities/Chat.entity';
+import { AdminModule } from './admin/admin.module';
 import { ChatController } from './Chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './Chat.service';
 
 @Module({
-  imports: [ConfigModule, MulterModule, TypeOrmModule.forFeature([Chat])],
+  imports: [
+    ConfigModule,
+    MulterModule,
+    TypeOrmModule.forFeature([Chat]),
+    AdminModule,
+  ],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
