0000f19f93f64a6914a982bdb10ed43e86911060
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-09 21:05:03 +0300

    added some imports

--- src/chat/Chat.module.ts
+++ src/chat/Chat.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Chat from 'src/entities/Chat.entity';
 import { ChatController } from './Chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './Chat.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, MulterModule, TypeOrmModule.forFeature([Chat])],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService],
 })
