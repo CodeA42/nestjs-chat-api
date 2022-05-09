59fdde5ea8ebb0aa4dc1d022c40d3088defce4fb
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-09 21:05:23 +0300

    create chat works now

--- src/chat/Chat.service.ts
+++ src/chat/Chat.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Request } from 'express';
+import Chat from 'src/entities/Chat.entity';
+import { createChatDto } from './dto/createChatDto';
+import { Repository } from 'typeorm';
+import User from 'src/entities/User.entity';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectRepository(Chat) private chatRepository: Repository<Chat>,
+  ) {}
+  createChat(chatData: createChatDto, req: Request) {
+    this.createNewChat(chatData.name, req.user.id);
+  }
+
+  createNewChat(chatName: string, adminId: unknown) {
+    const chat = new Chat();
+    chat.name = chatName;
+    chat.admin = adminId as User;
+
+    this.chatRepository.save(chat);
+  }
+}
