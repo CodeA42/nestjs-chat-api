92519e3c07e499c27d9e93572e96b7d604ef8816
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-09 13:08:03 +0300

    basic chat controller

--- src/chat/Chat.controller.ts
+++ src/chat/Chat.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  @Post('create_chat')
+  @UseInterceptors(FileInterceptor('image'))
+  createChat(@UploadedFile() image: Express.Multer.File) {
+    console.log(image);
+  }
+}
