aad69a905325558dc5f071c605a264987d2ba60e
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 16:52:53 +0300

    added change password handler

--- src/chat/Admin.service.ts
+++ src/chat/Admin.service.ts
@@ -3,11 +3,14 @@ import { Repository } from 'typeorm';
 import Chat from 'src/entities/Chat.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import User from 'src/entities/User.entity';
+import { NewPaswordDto } from 'src/dto/NewPasswordDto';
+import { ChatService } from './Chat.service';
 
 @Injectable()
 export class AdminService {
   constructor(
     @InjectRepository(Chat) private chatRepository: Repository<Chat>,
+    private chatService: ChatService,
   ) {}
 
   deleteRoom(roomId: string) {
@@ -24,5 +27,10 @@ export class AdminService {
     chat.adminId = userId;
 
     return this.chatRepository.save(chat);
+  }
+
+  changePassword(roomId: string, newPasswordDto: NewPaswordDto) {
+    if (this.chatService.checkPassword(roomId, newPasswordDto.oldPassword)) {
+    }
   }
 }
