fa5520cab9b09c5dc645fc49c8916316c76fc8bb
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 16:11:58 +0300

    path to get all chats moved UseGuard to the controller level

--- src/chat/Chat.controller.ts
+++ src/chat/Chat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthTypes } from 'src/@types/AuthTypes';
 import { Authentication } from 'src/decorators/Authentication.decorator';
 import { User } from 'src/decorators/User.decorator';
@@ -8,13 +8,19 @@ import { CreateChatDto } from '../dto/CreateChatDto';
 import { TokenUserDto } from 'src/dto/TokenUserDto';
 
 @Controller('chat')
+@UseGuards(AuthenticationGuard)
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post('create_chat')
   @Authentication(AuthTypes.ACCESS)
-  @UseGuards(AuthenticationGuard)
   createChat(@Body() newChatData: CreateChatDto, @User() user: TokenUserDto) {
     return this.chatService.createChat(newChatData, user.id);
+  }
+
+  @Get('/')
+  @Authentication(AuthTypes.ACCESS)
+  getAllChats() {
+    return this.chatService.getAllChats();
   }
 }
