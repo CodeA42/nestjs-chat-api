32560cd95f9b586bdea33af20aa3917ca4d9a017
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 16:13:10 +0300

    wrapping the chatId in dto to validate

--- src/guards/Authorization.guard.ts
+++ src/guards/Authorization.guard.ts
@@ -5,8 +5,10 @@ import {
   Inject,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { validate } from 'class-validator';
 import { Request } from 'express';
 import { ChatService } from 'src/chat/Chat.service';
+import { ChatIdDto } from 'src/dto/ChatIdDto';
 import User from 'src/entities/User.entity';
 
 @Injectable()
@@ -16,13 +18,22 @@ export class AuthorizationGurad implements CanActivate {
     @Inject(ChatService) private readonly chatService: ChatService,
   ) {}
 
-  async canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const req: Request = context.switchToHttp().getRequest();
 
-    const chatId: string = req.params.chatId;
-    const chatAdmin: User = await this.chatService.getChatAdmin(chatId);
+    let isAuthorized = false;
 
-    const isAuthorized: boolean = chatAdmin.id === req.user.id;
+    const chatIdDto = new ChatIdDto();
+    chatIdDto.id = req.params.chatId;
+    const validationErrors = await validate(chatIdDto);
+
+    if (validationErrors.length === 0) {
+      const chatAdmin: User = await this.chatService.getChatAdmin(chatIdDto.id);
+
+      isAuthorized = chatAdmin.id === req.user.id;
+    } else {
+      isAuthorized = false;
+    }
 
     return isAuthorized;
   }
