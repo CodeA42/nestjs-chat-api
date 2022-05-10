a1fe87a17fcd387832b2fc7ac446d649a6bd07a5
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 17:12:17 +0300

    was the wrong status code

--- src/exceptions/WrongCredentialsException.ts
+++ src/exceptions/WrongCredentialsException.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export default class WrongCredentialsException extends HttpException {
   constructor(message = 'Wrong credentials') {
-    super(message, HttpStatus.CONFLICT);
+    super(message, HttpStatus.FORBIDDEN);
     this.name = 'WrongCredentialsException';
   }
 }
