6a44b88e490667f37a6b46bc4d08e4e09515148a
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 10:13:15 +0300

    added authenticaiton and role metadata

--- src/chat/admin/Admin.controller.ts
+++ src/chat/admin/Admin.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
 import { AdminService } from './Admin.service';
+import { Roles } from '../../decorators/Roles.decorator';
+import { Authentication } from 'src/decorators/Authentication.decorator';
+import { AuthTypes } from 'src/@types/AuthTypes';
+import { AuthenticationGuard } from 'src/guards/Authentication.guard';
+import { AuthorizationGurad } from 'src/guards/Authorization.guard';
 
-@Controller('chat/:chatid/admin')
+@Controller('chat/:chatId/admin')
+@Roles('admin')
+@Authentication(AuthTypes.ACCESS)
+@UseGuards(AuthenticationGuard)
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
   @Delete('delete_room')
-  getRoute(@Param('chatid') id: string) {
+  getRoute(@Param('chatId') id: string) {
     return `Param: ${id}`;
+  }
+
+  @Get('transfer/:userId')
+  transferOwnership(
+    @Param('chatId') chatId: string,
+    @Param('userId') userId: string,
+  ) {
+    return this.adminService.transferOwnership(chatId, userId);
   }
 }
