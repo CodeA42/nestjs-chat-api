5ac56f3920686aaf2e116b6e49e4fab2c1e7fbba
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 14:27:09 +0300

    crud

--- src/chat/Chat.service.ts
+++ src/chat/Chat.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Request } from 'express';
 import Chat from 'src/entities/Chat.entity';
-import { CreateChatDto } from './dto/CreateChatDto';
+import { CreateChatDto } from '../dto/CreateChatDto';
 import { Repository } from 'typeorm';
 import User from 'src/entities/User.entity';
 import { TokenUser } from 'src/@types';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ChatService {
@@ -14,14 +18,67 @@ export class ChatService {
   ) {}
 
   createChat(chatData: CreateChatDto, userId: string) {
-    this.createNewChat(chatData.name, userId);
+    return this.createNewChat(chatData.name, userId);
   }
 
-  createNewChat(chatName: string, adminId: unknown) {
+  async createNewChat(chatName: string, adminId: string) {
     const chat = new Chat();
     chat.name = chatName;
-    chat.admin = adminId as User;
+    chat.adminId = adminId;
 
-    this.chatRepository.save(chat);
+    try {
+      return await this.chatRepository.save(chat);
+    } catch (e) {
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getChat(chatId: string): Promise<Chat> {
+    try {
+      const chat: Chat = await this.chatRepository.findOne(chatId);
+      if (chat) return chat;
+      throw new NotFoundException();
+    } catch (e) {
+      if (e instanceof NotFoundException) {
+        throw e;
+      }
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getChatWithAdmin(id: string): Promise<Chat> {
+    try {
+      const res = await this.chatRepository.findOne({
+        where: { id },
+        relations: ['admin'],
+      });
+      if (res) return res;
+      throw new NotFoundException();
+    } catch (e) {
+      if (e instanceof NotFoundException) {
+        throw e;
+      }
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getChatAdmin(id: string): Promise<User> {
+    try {
+      const chat: Chat = await this.chatRepository.findOne({
+        where: { id },
+        relations: ['admin'],
+      });
+      if (chat) return chat.admin;
+      throw new NotFoundException();
+    } catch (e) {
+      if (e instanceof NotFoundException) {
+        throw e;
+      }
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
   }
 }
