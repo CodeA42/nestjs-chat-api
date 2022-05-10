fae7a61a30352468ab25599e723904d99ef5222f
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 14:26:22 +0300

    uses dto's

--- src/chat/Chat.controller.ts
+++ src/chat/Chat.controller.ts
@@ -1,23 +1,20 @@
-import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common';
-import { TokenUser } from 'src/@types';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthTypes } from 'src/@types/AuthTypes';
 import { Authentication } from 'src/decorators/Authentication.decorator';
 import { User } from 'src/decorators/User.decorator';
 import { AuthenticationGuard } from 'src/guards/Authentication.guard';
-import { JoiValidationPipe } from 'src/validation/joi.validation';
 import { ChatService } from './Chat.service';
-import { CreateChatDto } from './dto/CreateChatDto';
-import createChatSchema from './validationSchemas/createChat.schema';
+import { CreateChatDto } from '../dto/CreateChatDto';
+import { TokenUserDto } from 'src/dto/TokenUserDto';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post('create_chat')
-  @UsePipes(new JoiValidationPipe(createChatSchema))
   @Authentication(AuthTypes.ACCESS)
   @UseGuards(AuthenticationGuard)
-  createChat(@Body() newChatData: CreateChatDto, @User('id') userId: string) {
-    this.chatService.createChat(newChatData, userId);
+  createChat(@Body() newChatData: CreateChatDto, @User() user: TokenUserDto) {
+    return this.chatService.createChat(newChatData, user.id);
   }
 }
