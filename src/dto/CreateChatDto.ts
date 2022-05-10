1a9fef8beb98ed679a4e2bfebf1e1450cb8460c9
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 16:12:09 +0300

    added password field

--- src/dto/CreateChatDto.ts
+++ src/dto/CreateChatDto.ts
@@ -1,6 +1,12 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsAlphanumeric, IsNotEmpty, Max, Min } from 'class-validator';
 
 export class CreateChatDto {
   @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  @IsAlphanumeric()
+  @Min(4)
+  @Max(20)
+  password: string;
 }
