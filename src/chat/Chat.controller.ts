22d8ae5ea24d043a8b12f05dc5bb183c9c7d39b8
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 16:50:07 +0300

    changed create_chat to just / added get /:id route

--- src/chat/Chat.controller.ts
+++ src/chat/Chat.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthTypes } from 'src/@types/AuthTypes';
 import { Authentication } from 'src/decorators/Authentication.decorator';
 import { User } from 'src/decorators/User.decorator';
@@ -12,7 +20,7 @@ import { TokenUserDto } from 'src/dto/TokenUserDto';
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
-  @Post('create_chat')
+  @Post('/')
   @Authentication(AuthTypes.ACCESS)
   createChat(@Body() newChatData: CreateChatDto, @User() user: TokenUserDto) {
     return this.chatService.createChat(newChatData, user.id);
@@ -22,5 +30,11 @@ export class ChatController {
   @Authentication(AuthTypes.ACCESS)
   getAllChats() {
     return this.chatService.getAllChats();
+  }
+
+  @Get('/:id')
+  @Authentication(AuthTypes.ACCESS)
+  getChatRoom(@Param('id', ParseUUIDPipe) id: string) {
+    return this.chatService.getChat(id);
   }
 }
