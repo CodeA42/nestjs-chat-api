d65043445741c973536d3135244de116c34b0baa
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-10 14:26:50 +0300

    no more admin module

--- src/chat/Chat.module.ts
+++ src/chat/Chat.module.ts
@@ -3,19 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Chat from 'src/entities/Chat.entity';
-import { AdminModule } from './admin/admin.module';
+import { AdminController } from './Admin.controller';
+import { AdminService } from './Admin.service';
 import { ChatController } from './Chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './Chat.service';
 
 @Module({
-  imports: [
-    ConfigModule,
-    MulterModule,
-    TypeOrmModule.forFeature([Chat]),
-    AdminModule,
-  ],
-  controllers: [ChatController],
-  providers: [ChatGateway, ChatService],
+  imports: [ConfigModule, MulterModule, TypeOrmModule.forFeature([Chat])],
+  controllers: [ChatController, AdminController],
+  providers: [ChatGateway, ChatService, AdminService],
 })
 export class ChatModule {}
