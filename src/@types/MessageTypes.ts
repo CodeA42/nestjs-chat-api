707fd9c8c1eb20a7dc901faa20f4553ba12fe181
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-11 17:22:47 +0300

    message types for websocket events

---
+++ src/@types/MessageTypes.ts
@@ -0,0 +1,8 @@
+export class MessageTypes {
+  static JOIN_ROOM = 'joinRoom';
+  static JOINED_ROOM = 'joinedRoom';
+  static LEAVE_ROOM = 'leaveRoom';
+  static LEFT_ROOM = 'leftRoom';
+  static MESSAGE_FROM_CLIENT = 'messageFromClient';
+  static MESSAGE_FROM_SERVER = 'messageFromServer';
+}
