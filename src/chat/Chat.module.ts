a02811a4f4f40f6d4d9a22880b2eca925dc4d26d
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-11 13:47:31 +0300

    user is needed in the join room handler

--- src/chat/Chat.module.ts
+++ src/chat/Chat.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Chat from 'src/entities/Chat.entity';
+import User from 'src/entities/User.entity';
 import { AdminController } from './Admin.controller';
 import { AdminService } from './Admin.service';
 import { ChatController } from './Chat.controller';
@@ -10,7 +11,7 @@ import { ChatGateway } from './chat.gateway';
 import { ChatService } from './Chat.service';
 
 @Module({
-  imports: [ConfigModule, MulterModule, TypeOrmModule.forFeature([Chat])],
+  imports: [ConfigModule, MulterModule, TypeOrmModule.forFeature([Chat, User])],
   controllers: [ChatController, AdminController],
   providers: [ChatGateway, ChatService, AdminService],
 })
