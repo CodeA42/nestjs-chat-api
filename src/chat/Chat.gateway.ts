4bd144f4e75d32fa3831eb31386a360756376c46
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-11 17:23:04 +0300

    using the message types class

--- src/chat/Chat.gateway.ts
+++ src/chat/Chat.gateway.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, ServerOptions, Socket } from 'socket.io';
 import { AuthTypes } from 'src/@types/AuthTypes';
+import { MessageTypes } from 'src/@types/MessageTypes';
 import { Authentication } from 'src/decorators/Authentication.decorator';
 import { AuthenticationGuard } from 'src/guards/Authentication.guard';
 
@@ -35,26 +36,26 @@ export class ChatGateway
 
   // @Authentication(AuthTypes.ACCESS)
   // @UseGuards(AuthenticationGuard)
-  @SubscribeMessage('messageToServer')
+  @SubscribeMessage(MessageTypes.MESSAGE_FROM_CLIENT)
   message(
     client: Socket,
     message: { sender: string; room: string; message: string },
   ) {
     // console.log(socket);
-    this.wss.to(message.room).emit('messageToClient', message);
+    this.wss.to(message.room).emit(MessageTypes.MESSAGE_FROM_SERVER, message);
 
     // return { event: 'message', data: message };
   }
 
-  @SubscribeMessage('joinRoom')
+  @SubscribeMessage(MessageTypes.JOIN_ROOM)
   joinRoom(client: Socket, room: string) {
     client.join(room);
-    client.emit('joinedRoom', room);
+    client.emit(MessageTypes.JOINED_ROOM, room);
   }
 
-  @SubscribeMessage('leaveRoom')
+  @SubscribeMessage(MessageTypes.LEAVE_ROOM)
   leaveRoom(client: Socket, room: string) {
     client.leave(room);
-    client.emit('leftRoom', room);
+    client.emit(MessageTypes.LEFT_ROOM, room);
   }
 }
