2bf326ebf29aa98c23c95361b173bf99b4791f2d
Aleksandar Velichkov <sashko98@gmail.com>
2022-05-11 13:52:07 +0300

    missing proprs for class-validator

--- src/dto/UserAuthDto.ts
+++ src/dto/UserAuthDto.ts
@@ -1,5 +1,13 @@
+import { IsAlphanumeric, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class UserAuthDto {
+  @IsAlphanumeric()
   username: string;
+
+  @IsNotEmpty()
+  @IsAlphanumeric()
   password: string;
+
+  @IsEmail()
   email: string;
 }
