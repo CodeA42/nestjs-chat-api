d4dca0165345c3882bb52e253a30fabc0fb232de
CodeA42 <sashko98@gmail.com>
2022-05-12 19:56:56 +0300

    new name auth setup

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,5 +1,13 @@
-import { Logger, UseGuards } from '@nestjs/common';
 import {
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Logger,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  BaseWsExceptionFilter,
   ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
@@ -8,18 +16,22 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsException,
   WsResponse,
 } from '@nestjs/websockets';
 import { Server, ServerOptions, Socket } from 'socket.io';
 import { AuthTypes } from 'src/@types/AuthTypes';
 import { MessageTypes } from 'src/@types/MessageTypes';
-import { Authentication } from 'src/decorators/Authentication.decorator';
+import { Authentication } from 'src/decorators/authentication.decorator';
 import { AuthenticationGuard } from 'src/guards/Authentication.guard';
+import { ChatService } from './chat.service';
 
 @WebSocketGateway(80, { cors: '*', namespace: '/chat' })
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(private chatService: ChatService) {}
+
   @WebSocketServer()
   wss: Server;
 
@@ -27,9 +39,20 @@ export class ChatGateway
   afterInit(server: Server) {
     this.logger.log('Initialized!');
   }
+
   handleConnection(client: Socket, ...args: any[]) {
+    console.log(client.connected);
+    if (client.handshake.auth.uuid !== '123') {
+      // this.wss.serverSideEmit('test');
+      // throw new HttpException('Invalid credentials.', HttpStatus.CONFLICT);
+      // this.wss.serverSideEmit(MessageTypes.DISCONNECT);
+      client.disconnect(true);
+      console.log(client.connected);
+      return;
+    }
     console.log(`Client connected: ${client.id}`);
   }
+
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
   }
@@ -45,6 +68,17 @@ export class ChatGateway
     this.wss.to(message.room).emit(MessageTypes.MESSAGE_FROM_SERVER, message);
 
     // return { event: 'message', data: message };
+  }
+
+  @SubscribeMessage(MessageTypes.CONNECTION)
+  connect(client: Socket, ...args: any[]) {
+    console.log('here');
+  }
+
+  @SubscribeMessage(MessageTypes.DISCONNECT)
+  disconnect(client: Socket, data: any) {
+    console.log(`Client disconnected: ${client.id}`);
+    console.log(data);
   }
 
   @SubscribeMessage(MessageTypes.JOIN_ROOM)
