a56c3febfbf962670dc43ccc7663f8c30574e7b6
CodeA42 <sashko98@gmail.com>
2022-05-13 13:01:33 +0300

    user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import User from '../entities/User.entity';
+import Token from '../entities/Token.entity';
+
+Injectable();
+export class UserService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async insertUser(username: string, password: string, email: string) {
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    user.email = email;
+
+    return await this.usersRepository.save(user);
+  }
+
+  async selectByEmail(email: string): Promise<User | null> {
+    try {
+      const user: User = await this.usersRepository.findOne({
+        where: {
+          email,
+        },
+      });
+      if (user) return user;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+    throw new NotFoundException();
+  }
+
+  async selectByUsername(username: string): Promise<User | null> {
+    try {
+      const user: User = await this.usersRepository.findOne({
+        where: {
+          username,
+        },
+      });
+      if (user) return user;
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+    throw new NotFoundException();
+  }
+}
