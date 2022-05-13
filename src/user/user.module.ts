536e70ef12f2d106f365258068d959bc72236109
CodeA42 <sashko98@gmail.com>
2022-05-13 13:01:29 +0300

    user module

---
+++ src/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from '../entities/User.entity';
+import { UserService } from './user.service';
+
+@Global()
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [],
+  providers: [],
+  exports: [UserService],
+})
+export class UserModule {}
