f3d818c2bff74817448f4ee132c84e0898f28f00
CodeA42 <sashko98@gmail.com>
2022-05-13 13:10:13 +0300

    user is no longer needed

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,13 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Token from '../entities/Token.entity';
-import User from '../entities/User.entity';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Token]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([Token]), ConfigModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
   exports: [AuthenticationService],
