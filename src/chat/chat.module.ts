30078ede14f7d6c23c6f6902c29ccd1bbe2ab8f7
CodeA42 <sashko98@gmail.com>
2022-05-13 13:10:25 +0300

    message service was missing

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,21 +3,23 @@ import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Chat from 'src/entities/Chat.entity';
+import Message from 'src/entities/Message.entity';
 import User from 'src/entities/User.entity';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { MessageService } from './message.service';
 
 @Module({
   imports: [
     ConfigModule,
     MulterModule,
-    TypeOrmModule.forFeature([Chat, User]),
+    TypeOrmModule.forFeature([Chat, User, Message]),
     CacheModule.register(),
   ],
   controllers: [ChatController, AdminController],
-  providers: [ChatGateway, ChatService, AdminService],
+  providers: [ChatGateway, ChatService, AdminService, MessageService],
 })
 export class ChatModule {}
