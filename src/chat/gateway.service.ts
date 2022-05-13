cb9bbcb8c0cbc5cd87079660e07f2c3ad1a97af5
CodeA42 <sashko98@gmail.com>
2022-05-13 14:14:54 +0300

    aaand its obsolete

--- src/chat/gateway.service.ts
+++ src/chat/gateway.service.ts
@@ -1,12 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { validate } from 'class-validator';
-import { MessageDataDto } from 'src/dto/MessageDataDto';
-
 Injectable();
-export class GatewayService {
-  async validateMessageData(messageData: MessageDataDto): Promise<boolean> {
-    const message = new MessageDataDto(messageData);
-    const errors = await validate(message);
-    return errors.length === 0;
-  }
-}
+export class GatewayService {}
