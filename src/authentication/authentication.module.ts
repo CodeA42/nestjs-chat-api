0817b6a7963f35dae3bc37a25fbb21ab67bb747f
CodeA42 <sashko98@gmail.com>
2022-05-13 13:24:01 +0300

    User is need after all UserService was missing

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,15 +1,17 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import User from 'src/entities/User.entity';
+import { UserService } from 'src/user/user.service';
 import Token from '../entities/Token.entity';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([Token]), ConfigModule],
+  imports: [TypeOrmModule.forFeature([Token, User]), ConfigModule],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, UserService],
   exports: [AuthenticationService],
 })
 export class AuthenticationModule {}
