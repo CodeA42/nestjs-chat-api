411e89e66b3768f0d2dbc9f4a8c9d5bb0e0f6d20
CodeA42 <sashko98@gmail.com>
2022-05-13 13:25:02 +0300

    MessageDataDto

---
+++ src/dto/MessageDataDto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class MessageDataDto {
+  @IsNotEmpty()
+  @IsUUID()
+  sender: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  room: string;
+
+  @IsNotEmpty()
+  @Length(0, 2000)
+  body;
+}
