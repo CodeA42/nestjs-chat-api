2066f1ff4d82e80f2d3d95cabc6d6f62eddec28c
CodeA42 <sashko98@gmail.com>
2022-05-13 14:02:17 +0300

    wsexception filter

---
+++ src/filters/WsExceptionFiler.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-types */
+import { ArgumentsHost } from '@nestjs/common';
+import { BaseWsExceptionFilter } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { Events } from 'src/@types/Events';
+
+export class WsExceptionFilter implements BaseWsExceptionFilter {
+  catch(exception: any, host: ArgumentsHost): void {
+    const client: Socket = host.switchToWs().getClient();
+    const data = host.switchToWs().getData();
+    client.emit(Events.IMPROPER_MESSAGE_FORMAT, {
+      data,
+      error: exception.name,
+    });
+  }
+  handleError<TClient extends { emit: Function }>(
+    client: TClient,
+    exception: any,
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+  handleUnknownError<TClient extends { emit: Function }>(
+    exception: any,
+    client: TClient,
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+  isExceptionObject(err: any): err is Error {
+    throw new Error('Method not implemented.');
+  }
+}
