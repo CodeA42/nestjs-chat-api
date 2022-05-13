5eaaf958f10ab7570b919c3d3a7abf8479b07d36
CodeA42 <sashko98@gmail.com>
2022-05-13 15:19:15 +0300

    allowing for the same field with different names

--- src/dto/MessageDataDto.ts
+++ src/dto/MessageDataDto.ts
@@ -1,13 +1,25 @@
-import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+import { IsNotEmpty, IsUUID, Length, ValidateIf } from 'class-validator';
 
 export class MessageDataDto {
   @IsNotEmpty()
   @IsUUID()
-  sender: string;
+  @ValidateIf((o) => o.userId === undefined || o.sender)
+  sender?: string;
 
   @IsNotEmpty()
   @IsUUID()
-  room: string;
+  @ValidateIf((o) => o.sender === undefined || o.userId)
+  userId?: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  @ValidateIf((o) => o.chatId === undefined || o.room)
+  room?: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  @ValidateIf((o) => o.room === undefined || o.chatId)
+  chatId?: string;
 
   @IsNotEmpty()
   @Length(0, 2000)
