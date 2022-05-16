fbcb4ea281bd5864ae591cdea1666a1f367b205a
CodeA42 <sashko98@gmail.com>
2022-05-16 10:51:08 +0300

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Pytek chat API')
+    .setDescription("Pytek's awesome chat api")
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
